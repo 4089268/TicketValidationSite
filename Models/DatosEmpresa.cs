e7ae9881acb36d74c44a7a0f2c3e80fed608b699
Salvador Rangel A <chava_r17@hotmail.com>
2025-10-03 10:27:20 -0600

    Se removio la direccion de la cabecera

--- Models/DatosEmpresa.cs
+++ Models/DatosEmpresa.cs
@@ -5,8 +5,8 @@ namespace TicketValidationSite.Models;
 public class DatosEmpresa
 {
     public string RazonSocial { get; set; } = "COMISION DEL AGUA DEL ESTADO DE VERACRUZ";
-    public string Direccion1 { get; set; } = "ABASOLO S/N, COL. CENTRO";
-    public string Direccion2 { get; set; } = "CP. 95641, CD ISLA VERACRUZ";
+    public string? Direccion1 { get; set; }
+    public string? Direccion2 { get; set; }
     public string? RFC { get; set; }
     public string? Telefono { get; set; }
 
